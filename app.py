5187a5eef75e1b3e0e872c34f416f5cbd59739f0
danielpraburaj <apdaniel@stanford.edu>
2025-03-14 18:53:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,8 @@ def predict():
 
     return jsonify({"prediction": result})
 
+import os
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 8080))  # Render provides PORT dynamically
+    app.run(host="0.0.0.0", port=port)
